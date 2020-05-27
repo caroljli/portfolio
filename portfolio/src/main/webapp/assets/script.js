25921b05c08bdd06af936b759822cf829babdab9
Carol Li <caroldotli@gmail.com>
2020-05-27 14:31:36 -0400

    Make functional slider, blog post page

--- portfolio/src/main/webapp/assets/script.js
+++ portfolio/src/main/webapp/assets/script.js
@@ -26,3 +26,20 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function collapseMenu() {
+    var i;
+    var elements = document.getElementsByClassName("collapsible");
+
+    for (i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+}
