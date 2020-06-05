025e8857a50f5f6e97b86baa7869c78a1124d1f2
Carol Li <caroldotli@gmail.com>
2020-06-05 15:52:51 +0000

    Add untracked files and remove unneeded fields

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,22 @@
+package com.google.sps.data;
+
+import java.util.*;
+import com.google.sps.data.Reply;
+
+/** A comment on the forum page. */
+public final class Comment {
+
+  private final long id;
+  private final String name;
+  private final String comment;
+  private final String email;
+  private final String date;
+
+  public Comment(long id, String name, String comment, String email, String date) {
+    this.id = id;
+    this.name = name;
+    this.comment = comment;
+    this.email = email;
+    this.date = date;
+  }
+}
