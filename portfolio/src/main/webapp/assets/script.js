df63a6b0c6d49cfcc2bea67d8bf924daad7b24e2
Carol Li <caroldotli@gmail.com>
2020-06-09 22:50:24 +0000

    Add map embedding to page

--- portfolio/src/main/webapp/assets/script.js
+++ portfolio/src/main/webapp/assets/script.js
@@ -345,3 +345,14 @@ function deleteAllComments() {
     getComments();
   });
 }
+
+/**
+ * Creates a Google Map and adds it to the page.
+ */
+function createMap() {
+  const map = new google.maps.Map(
+    document.getElementById('map'),
+    {center: {lat: 37.422, lng: -122.084}, zoom: 16}
+  );
+  console.log("created map");
+}
