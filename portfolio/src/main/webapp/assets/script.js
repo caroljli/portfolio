332d3c6b4c5fe9b93e4a501ffc0edae7fb1c10f2
Carol Li <caroldotli@gmail.com>
2020-06-10 15:49:31 +0000

    Implemented MarkerServlet and click commenting feature

--- portfolio/src/main/webapp/assets/script.js
+++ portfolio/src/main/webapp/assets/script.js
@@ -346,13 +346,185 @@ function deleteAllComments() {
   });
 }
 
+let map;
+let markerTemp;
+
 /**
- * Creates a Google Map and adds it to the page.
+ * Creates a Google Map that allows user to add markers and adds it to the page.
  */
 function createMap() {
-  const map = new google.maps.Map(
+  map = new google.maps.Map(
     document.getElementById('map'),
-    {center: {lat: 37.422, lng: -122.084}, zoom: 16}
+    {
+      center: {lat: 37.422, lng: -122.084}, 
+      zoom: 8,
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
+    }
   );
   console.log("created map");
+
+  map.addListener('click', (event) => {
+    createEditableMarker(event.latLng.lat(), event.latLng.lng());
+  });
+
+  fetchMarkers();
+}
+
+/**
+ * Creates a marker that shows read-only information.
+ */
+function createMarker(lat, lng, content) {
+  const marker = new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  const contentWindow = new google.maps.InfoWindow({content: content});
+  marker.addListener('click', () => {
+    contentWindow.open(map, marker);
+  });
+}
+
+/**
+ * Creates marker that shows a textbook for input.
+ */
+function createEditableMarker(lat, lng) {
+  // If an editable marker is already showing, remove it.
+  if (markerTemp) {
+    markerTemp.setMap(null);
+  }
+
+  markerTemp = new google.maps.Marker({position: {lat: lat, lng: lng}, map: map});
+
+  const contentWindow = new google.maps.InfoWindow({content: buildInput(lat, lng)});
+
+  // When the user closes the editable info window, remove the marker.
+  google.maps.event.addListener(contentWindow, 'closeclick', () => {
+    markerTemp.setMap(null);
+  });
+
+  contentWindow.open(map, markerTemp);
+}
+
+/**
+ * Sends a marker to the backend for saving.
+ */
+function postMarker(lat, lng, content) {
+  const params = new URLSearchParams();
+  params.append('lat', lat);
+  params.append('lng', lng);
+  params.append('content', content);
+
+  fetch('/get-markers', {method: 'POST', body: params});
+}
+
+/**
+ * Fetches all markers and adds to map.
+ */
+function fetchMarkers() {
+  fetch('/get-markers').then(response => response.json()).then((markers) => {
+    markers.forEach(
+      (marker) => {
+        createMarker(marker.lat, marker.lng, marker.content)
+      }
+    );
+  });
+}
+
+/**
+ * Builds editable textbox elements with submit button.
+ */
+function buildInput(lat, lng, content) {
+  const textBox = document.createElement('textarea');
+  const button = document.createElement('button');
+  button.appendChild(document.createTextNode('Submit'));
+
+  button.onclick = () => {
+    postMarker(lat, lng, textBox.value);
+    createMarkerForDisplay(lat, lng, textBox.value);
+    editMarker.setMap(null);
+  };
+
+  const containerDiv = document.createElement('div');
+  containerDiv.appendChild(textBox);
+  containerDiv.appendChild(document.createElement('br'));
+  containerDiv.appendChild(button);
+
+  return containerDiv;
 }
