c224a4ed0ee4babe94fff08382b85dd566b3e750
Carol Li <caroldotli@gmail.com>
2020-06-16 15:59:18 +0000

    Implement get unavailable times

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,56 @@
 
 package com.google.sps;
 
+import java.util.*;
 import java.util.Collection;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    // Meeting is longer than the whole day, return an empty array.
+    if (request.getDuration() >= TimeRange.WHOLE_DAY.duration()) {
+      return Arrays.asList();
+    }
+
+    // If there are no events, return the whole day as time range.
+    if (events.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    Collection<TimeRange> result;
+    Collection<String> requestAttendees = request.getAttendees();
+    Collection<String> requestAttendeesOptional = request.getOptionalAttendees();
+
+    // If there are no attendees, return the whole day.
+    if (requestAttendees.isEmpty() && requestAttendeesOptional.isEmpty()) {
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+  }
+
+  /**
+   * Get all unavailable times in which meeting request attendees are busy.
+   * 
+   * @param all events to get unavailabilities from
+   * @param request attendees to find overlap for
+   * @return a set of time ranges that are already occupied
+   */
+  public Set<TimeRange> getUnavailableTimes(Collection<Event> events, Collection<String> requestAttendees) {
+    Set<TimeRange> output = new HashSet<>();
+
+    for (Event e : events) {
+      Set<String> eventAttendees = e.getAttendees();
+      
+      for (String eventAttendee : eventAttendees) {
+
+        // Checks to see if event has overlap with the attendees of the request
+        if (requestAttendees.contains(eventAttendee)) {
+          output.add(e.getWhen());
+          break; // Only one attendee is required for the event time to be unavailable. 
+        }
+      }
+    }
+    
+    return output;
   }
 }
